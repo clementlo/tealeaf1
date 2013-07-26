283dfee45d0c83dbf902b27da6ee181def9d7042
Clement Lo <clementlo@gmail.com>
2013-07-26 09:56:27 -0400

    Add card images and name validation

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -50,7 +50,26 @@ helpers do
     end
     total
   end
-    # calculate_total(session[:dealer_cards])
+
+  def card_image(card)
+    suit = case card[0]
+      when 'H' then 'hearts'
+      when 'D' then 'diamonds'
+      when 'C' then 'clubs'
+      when 'S' then 'spades'
+    end
+
+    value = card[1]
+    if ['J','Q', 'K', 'A'].include?(value)
+      value = case card[1]
+        when 'J' then 'jack'
+        when 'Q' then 'queen'
+        when 'K' then 'king'
+        when 'A' then 'ace'
+      end
+    end
+    "<img src= '/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+  end
 end
 
 before do 
@@ -70,6 +89,11 @@ get '/new_player' do
 end
 
 post '/new_player' do
+  if params[:player_name].empty?
+    @error = "Name is required"
+    halt erb(:new_player)
+  end
+
   session[:player_name] = params[:player_name]
   redirect '/game'
 end
@@ -92,7 +116,7 @@ get '/game' do
   session[:player_cards] << session[:deck].pop
 
  if calculate_total(session[:player_cards]) == 21
-    @success = "Congratulations, you hit blackjack!"
+    @success = "Congratulations, #{session[:player_name]} hit blackjack!"
     @show_buttons = false
   end
   erb :game
@@ -101,35 +125,38 @@ end
 #player turn - hit or stay
 post '/game/player/hit' do
   session[:player_cards] << session[:deck].pop
-    if calculate_total(session[:player_cards]) > 21
-      @error = "Sorry, it looks like you busted"
+    player_total = calculate_total(session[:player_cards])
+    if player_total > 21
+      @error = "Sorry, it looks like #{session[:player_name]} busted"
       @show_buttons = false
-    elsif calculate_total(session[:player_cards]) == 21
-    @success = "Congratulations, you hit blackjack!"
+    elsif player_total == 21
+    @success = "Congratulations, #{session[:player_name]} hit blackjack!"
     @show_buttons = false
     end
   erb :game
 end
 
 post '/game/player/stay' do
-  @success = "You have chosen to stay."
+  @success = "#{session[:player_name]} has chosen to stay."
   @show_buttons = false
+  # dealer_total = calculate_total(session[:dealer_cards])
+  # player_total = calculate_total(session[:player_cards])
   if calculate_total(session[:dealer_cards]) == 21 
-    @error = "Sorry, the dealer hit blackjack. You lose!"
+    @error = "Sorry, the dealer hit blackjack. #{session[:player_name]} loses!"
   end
   while calculate_total(session[:dealer_cards]) < 17
     session[:dealer_cards] << session[:deck].pop
     if calculate_total(session[:dealer_cards]) == 21 
-      @error = "Sorry, the dealer hit blackjack. You lose!"
+      @error = "Sorry, the dealer hit blackjack. #{session[:player_name]} loses!"
     elsif calculate_total(session[:dealer_cards]) > 21 
-      @success = "The dealer has busted! You Win!"
+      @success = "The dealer has busted! #{session[:player_name]} wins!"
     end
   end
   if calculate_total(session[:dealer_cards]) < 21
-    if calculate_total(session[:dealer_cards]) > calculate_total(session[:player_cards]) 
+    if calculate_total(session[:dealer_cards]) > calculate_total(session[:player_cards])
       @error = "Sorry, the dealer wins!"
-    elsif calculate_total(session[:dealer_cards]) < calculate_total(session[:player_cards]) 
-      @success = "You win!"
+    elsif calculate_total(session[:dealer_cards]) < calculate_total(session[:player_cards])
+      @success = "#{session[:player_name]} wins!"
     else
       @success = "It's a tie!"
     end
